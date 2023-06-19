8bae5adf68a04229ce89224d9d3a7fd9a93d84b7
brian <knoooooooow@outlook.com>
2023-06-20 03:11:51 +0800

    init

--- src/app/main/audio-visualizer/audio-visualizer.component.ts
+++ src/app/main/audio-visualizer/audio-visualizer.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from "gsap";
+import * as dat from "dat.gui";
+
 @Component({
     selector: 'app-audio-visualizer',
     templateUrl: './audio-visualizer.component.html',
@@ -16,6 +19,7 @@ export class AudioVisualizerComponent implements OnInit {
         1000
     );
     renderer = new THREE.WebGLRenderer();
+    controls = new OrbitControls(this.camera,this.renderer.domElement)
     @ViewChild('audioVisualizer', { static: true }) audioVisualizer: ElementRef | undefined;
     constructor() { }
 
@@ -27,20 +31,49 @@ export class AudioVisualizerComponent implements OnInit {
         const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
         const cube = new THREE.Mesh(geometry, material);
 
-        cube.scale.set(3,2,1);
+        let animate1 = gsap.to(cube.position, {
+            x: 2 * Math.PI, duration: 5, ease: "power1.inOut", yoyo: true, onStart: () => {
+                console.log('动画开始');
+            }
+        });
+
+        const gui = new dat.GUI();
+        gui.add(cube.position,"x").min(0).max(5).step(0.1).name('cube').onChange((value)=>{
+
+        })
+        gui.addColor({color:"#ffff00"},"color").onChange((value)=>{
+            cube.material.color.set(value);
+        })
+
+
+        window.addEventListener("dblclick",()=>{
+            const fullScreenElement = document.fullscreenElement;
+            if(!fullScreenElement){
+                this.renderer.domElement.requestFullscreen();
+            }else{
+                document.exitFullscreen();
+            }
+        });
+        window.addEventListener("resize",()=>{
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            this.camera.updateProjectionMatrix();
+            this.renderer.setSize(window.innerWidth , window.innerHeight);
+            this.renderer.setPixelRatio(window.devicePixelRatio)
+        });
+
         this.scene.add(cube);
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.audioVisualizer?.nativeElement.appendChild(this.renderer.domElement);
         // this.renderer.render(this.scene, this.camera)
         this.render();
-        const controls = new OrbitControls(this.camera, this.renderer.domElement);
 
         const axesHelper = new THREE.AxesHelper(5);
         this.scene.add(axesHelper);
     }
 
     render() {
+        this.controls.update();
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(this.render.bind(this));
     }
