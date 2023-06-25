f2b71de14a4c7c14b7e7bcaf605944a07b8beef0
brian <knoooooooow@outlook.com>
2023-06-26 04:16:44 +0800

    init cubes

--- src/app/main/audio-visualizer/audio-visualizer.component.ts
+++ src/app/main/audio-visualizer/audio-visualizer.component.ts
@@ -19,56 +19,31 @@ export class AudioVisualizerComponent implements OnInit {
         1000
     );
     renderer = new THREE.WebGLRenderer();
-    controls = new OrbitControls(this.camera,this.renderer.domElement)
+    controls = new OrbitControls(this.camera, this.renderer.domElement)
     @ViewChild('audioVisualizer', { static: true }) audioVisualizer: ElementRef | undefined;
     constructor() { }
 
     ngOnInit() {
-        this.camera.position.set(0, 0, 10);
+        this.camera.position.set(0, 0, 300);
         this.scene.add(this.camera);
 
-        const geometry = new THREE.BoxGeometry(1, 1, 1);
-        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-        const cube = new THREE.Mesh(geometry, material);
-
-        let animate1 = gsap.to(cube.position, {
-            x: 2 * Math.PI, duration: 5, ease: "power1.inOut", yoyo: true, onStart: () => {
-                console.log('动画开始');
-            }
-        });
-
-        const gui = new dat.GUI();
-        gui.add(cube.position,"x").min(0).max(5).step(0.1).name('cube').onChange((value)=>{
-
-        })
-        gui.addColor({color:"#ffff00"},"color").onChange((value)=>{
-            cube.material.color.set(value);
-        })
-
-
-        window.addEventListener("dblclick",()=>{
-            const fullScreenElement = document.fullscreenElement;
-            if(!fullScreenElement){
-                this.renderer.domElement.requestFullscreen();
-            }else{
-                document.exitFullscreen();
-            }
-        });
-        window.addEventListener("resize",()=>{
+        window.addEventListener("resize", () => {
             this.camera.aspect = window.innerWidth / window.innerHeight;
             this.camera.updateProjectionMatrix();
-            this.renderer.setSize(window.innerWidth , window.innerHeight);
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
             this.renderer.setPixelRatio(window.devicePixelRatio)
         });
-
-        this.scene.add(cube);
+        const cubes = this.generateBoxGeometry({ width: 1, height: 5, depth: 1 }, { color: 0x00ff00 }, { spacing: 1, unit: 50, startPosition: { x: 10, y: 10, z: 10 } });
+        cubes.forEach(cube => {
+            this.scene.add(cube);
+        })
 
         this.renderer.setSize(window.innerWidth, window.innerHeight);
         this.audioVisualizer?.nativeElement.appendChild(this.renderer.domElement);
         // this.renderer.render(this.scene, this.camera)
         this.render();
 
-        const axesHelper = new THREE.AxesHelper(5);
+        const axesHelper = new THREE.AxesHelper(200);
         this.scene.add(axesHelper);
     }
 
@@ -77,5 +52,43 @@ export class AudioVisualizerComponent implements OnInit {
         this.renderer.render(this.scene, this.camera);
         requestAnimationFrame(this.render.bind(this));
     }
+    generateBoxGeometry(boxGeometry: {
+        width: number,
+        height: number,
+        depth: number,
+    }, meshBasicMaterial: {
+        color?: THREE.ColorRepresentation | undefined;
+    },
+        params: { spacing: number, unit: number, startPosition: { x: number, y: number, z: number } }): THREE.Mesh[] {
 
+        let cubeList: THREE.Mesh[] = [];
+        let unit = params.unit;
+        let position = { x: params.startPosition.x, y: params.startPosition.y, z: params.startPosition.z };
+
+        for (let i = 0; i < unit; i++) {
+            const geometry = new THREE.BoxGeometry(boxGeometry.width, boxGeometry.height, boxGeometry.depth);
+            const material = new THREE.MeshBasicMaterial({ color: meshBasicMaterial.color });
+            const cube = new THREE.Mesh(geometry, material);
+
+            const x = position.x + (boxGeometry.width + params.spacing) * i;
+            const y = position.y;
+            const z = position.z;
+            cube.position.set(x, y, z);
+
+            cubeList.push(cube);
+        }
+
+        return cubeList;
+    }
+
+    fullScreen() {
+        window.addEventListener("dblclick", () => {
+            const fullScreenElement = document.fullscreenElement;
+            if (!fullScreenElement) {
+                this.renderer.domElement.requestFullscreen();
+            } else {
+                document.exitFullscreen();
+            }
+        });
+    }
 }
